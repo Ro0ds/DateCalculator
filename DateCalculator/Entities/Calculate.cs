28bd3233310e7a5d87b27ee339476846972f1385
Digo <digo_gabriel97@yahoo.com.br>
2022-02-23 15:36:07 -0300

    Adicionar arquivos de projeto.

---
+++ DateCalculator/Entities/Calculate.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace DateCalculator.Entities {
+    class Calculate {
+        public double Years { get; set; }
+        public double Months { get; set; }
+        public double Weeks { get; set; }
+        public double Days { get; set; }
+
+        public Calculate() { }
+
+        public void Calc(double value, char choice) {
+            double daysYear = 365;
+
+            /*
+             1 - Years 
+             2 - Months
+             3 - Weeks
+             4 - Days
+             */
+
+            if (choice == '1') {
+                Years = value;
+                Months = value * 12;
+                Days = daysYear * value;
+                Weeks = Days / 7;
+            }
+            else if (choice == '2') {
+                Months = value;
+                Days = value * 30.4;
+                Years = value / 12;
+                Weeks = Days / 7;
+            }
+            else if (choice == '3') {
+                Weeks = value;
+                Days = value * 7;
+                Years = Days / daysYear;
+                Months = Days / 30.4;
+            }
+            else if (choice == '4') {
+                Days = value;
+                Weeks = value / 7;
+                Years = value / daysYear;
+                Months = value / 30.4;
+            }
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.AppendLine($"Years: {Years.ToString("F1", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Months: {Months.ToString("F1", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Weeks: {Weeks.ToString("F1", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Days: {Days.ToString("F1", CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DateCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using DateCalculator.Entities;
+
+namespace DateCalculator {
+    class Program {
+        static void Main(string[] args) {
+            char ch;
+            double value;
+
+            Calculate calculate = new Calculate();
+
+            start:
+            Console.Clear();
+            Console.WriteLine("||------------||");
+            Console.WriteLine("|| 1 - Years  ||");
+            Console.WriteLine("|| 2 - Months ||");
+            Console.WriteLine("|| 3 - Weeks  ||");
+            Console.WriteLine("|| 4 - Days   ||");
+            Console.WriteLine("||------------||");
+
+            Console.WriteLine();
+            Console.Write("Choice: ");
+            ch = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (ch == '1') {
+                Console.Write("Years: ");
+                value = double.Parse(Console.ReadLine());
+                calculate.Calc(value, ch);
+            }
+            else if (ch == '2') {
+                Console.Write("Months: ");
+                value = double.Parse(Console.ReadLine());
+                calculate.Calc(value, ch);
+            }
+            else if (ch == '3') {
+                Console.Write("Weeks: ");
+                value = double.Parse(Console.ReadLine());
+                calculate.Calc(value, ch);
+            }
+            else if (ch == '4') {
+                Console.Write("Days: ");
+                value = double.Parse(Console.ReadLine());
+                calculate.Calc(value, ch);
+            }
+            else {
+                MessageBox.Show("Wrong number, try again");
+            }
+
+            Console.WriteLine(calculate);
+
+            Console.WriteLine();
+            Console.Write("Again? (y/n): ");
+            char ch1 = char.Parse(Console.ReadLine());
+
+            if (ch1 == 'y') {
+                goto start;
+            }
+        }
+    }
+}
